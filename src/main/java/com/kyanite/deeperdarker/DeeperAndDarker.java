4c8db67b2a42d40822b91aa0d269754123d34f93
ang-xd <angelo.barone2010@icloud.com>
2022-08-24 21:22:05 -0400

    Base

---
+++ src/main/java/com/kyanite/deeperdarker/DeeperAndDarker.java
@@ -0,0 +1,10 @@
+package com.kyanite.deeperdarker;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DeeperAndDarker implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/kyanite/deeperdarker/client/DeeperAndDarkerClient.java
@@ -0,0 +1,13 @@
+package com.kyanite.deeperdarker.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class DeeperAndDarkerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
